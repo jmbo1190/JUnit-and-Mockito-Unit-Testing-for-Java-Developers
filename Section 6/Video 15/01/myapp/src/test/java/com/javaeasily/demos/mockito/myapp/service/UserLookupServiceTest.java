53f4b744139f55569b86071514097f056aa3f289
Jean-Michel Bodart <jmbodart@skynet.be>
2022-12-29 21:25:06 +0100

    assert actualUsers rather than userList (not obtained through testing)

--- Section 6/Video 15/01/myapp/src/test/java/com/javaeasily/demos/mockito/myapp/service/UserLookupServiceTest.java
+++ Section 6/Video 15/01/myapp/src/test/java/com/javaeasily/demos/mockito/myapp/service/UserLookupServiceTest.java
@@ -1,14 +1,16 @@
 package com.javaeasily.demos.mockito.myapp.service;
 
 import com.javaeasily.demos.mockito.myapp.User;
+import com.javaeasily.demos.mockito.myapp.User.UserType;
 import com.javaeasily.demos.mockito.myapp.data.UserRepository;
-import org.junit.Assert;
+//import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+//import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
@@ -39,12 +41,32 @@ public class UserLookupServiceTest {
         Set<User> actualUsers = userLookupService.getRegularUsers();
 
         // assert
+        // general collection
         assertNotNull(actualUsers);
-        assertEquals(actualUsers.size(), 2);
+        assertEquals(2, actualUsers.size());
+
+        // loop through users
         for (User actualUser : actualUsers) {
             assertNotNull(actualUser);
-            assertEquals(actualUser.getUserType(), User.UserType.REGULAR_USER);
+            assertEquals(User.UserType.REGULAR_USER, actualUser.getUserType());
         }
+
+        // User 1
+        Object[] actualUsersArray = actualUsers.toArray();
+
+        User ActualUser1 = (User) actualUsersArray[0];
+        assertNotNull(ActualUser1);
+        assertEquals(UserType.REGULAR_USER, ActualUser1.getUserType());
+        assertNotNull(ActualUser1.getUsername());
+        assertNotNull(ActualUser1.getPassword());
+
+        // User 2
+        User ActualUser2 = (User) actualUsersArray[1];
+        assertNotNull(ActualUser2);
+        assertEquals(UserType.REGULAR_USER, ActualUser2.getUserType());
+        assertNotNull(ActualUser2.getUsername());
+        assertNotNull(ActualUser2.getPassword());
+        
     }
 
     @Test
