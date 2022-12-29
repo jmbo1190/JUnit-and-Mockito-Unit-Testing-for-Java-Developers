4f14bcc143f6b80bff30f3082fa96120df876e57
Jean-Michel Bodart <jmbodart@skynet.be>
2022-12-29 18:25:25 +0100

    Ignore specific test case

--- Section 6/Video 02/myapp/src/test/java/com/javaeasily/demos/mockito/myapp/service/AuthenticationServiceTest.java
+++ Section 6/Video 02/myapp/src/test/java/com/javaeasily/demos/mockito/myapp/service/AuthenticationServiceTest.java
@@ -2,6 +2,7 @@ package com.javaeasily.demos.mockito.myapp.service;
 
 import com.javaeasily.demos.mockito.myapp.data.UserRepository;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -17,6 +18,7 @@ public class AuthenticationServiceTest {
         this.service = new AuthenticationService(repository);
     }
 
+    @Ignore
     @Test
     public void testAuthenticate() {
         // arrange
